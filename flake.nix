07e6e4dbe8fc19e91a41c01993fb70464e5109d9
Florian Marrero Liestmann <f.m.liestmann@fx-ttr.de>
2024-12-05 19:26:28 +0100

    Add a tofu wrapper

--- flake.nix
+++ flake.nix
@@ -6,26 +6,33 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, sops-nix, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system: let 
-      pkgs = import nixpkgs { inherit system; }; 
-    in 
-    {
-      devShell = pkgs.mkShell {
-        buildInputs = [
-          pkgs.k9s
-          pkgs.opentofu
-          pkgs.nano
-          pkgs.sops
-        ];
+  outputs = { self, nixpkgs, sops-nix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        tofu = pkgs.writeShellScriptBin "tofu" ''
+          #!/usr/bin/env bash
+          set -euo pipefail
 
-        shellHook = ''
-          function tofu {
-            sops --decrypt terraform.enc.tfstate > terraform.tfstate
-            tofu $@
+          if [ ! -f terraform.enc.tfstate ]; then
+            ${pkgs.opentofu}/bin/tofu "$@"
             sops --encrypt terraform.tfstate > terraform.enc.tfstate
-          }
+            exit 1
+          fi
+
+          sops --decrypt terraform.enc.tfstate > terraform.tfstate
+          ${pkgs.opentofu}/bin/tofu "$@"
+          sops --encrypt terraform.tfstate > terraform.enc.tfstate
         '';
-      };
-    });
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            tofu
+            pkgs.k9s
+            pkgs.nano
+            pkgs.sops
+          ];
+        };
+      });
 }
